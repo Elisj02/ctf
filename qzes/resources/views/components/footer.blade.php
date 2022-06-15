8669ee02bb7aadfd9a7d4998a369d9492364012e
elisj2002 <eserjim02@iesmarquesdecomares.org>
2022-06-15 12:23:47 +0200

    enlaces

--- qzes/resources/views/components/footer.blade.php
+++ qzes/resources/views/components/footer.blade.php
@@ -1,19 +1,24 @@
 <footer>
-  <div class="container-fluid footer">
-    <div class="justify-content-center">
-      <img alt="" class="sector2-1Fot"
-        src="https://static.overlay-tech.com/assets/71e9f76a-bafc-4aa8-a208-97e3051dd4a9.png" />
+    <div class="container-fluid footer">
+        <div class="justify-content-center">
+            <img alt="" class="sector2-1Fot"
+                src="https://static.overlay-tech.com/assets/71e9f76a-bafc-4aa8-a208-97e3051dd4a9.png" />
+        </div>
+        <div class="group-6 justify-content-center">
+            <p class="politica-de-datos">Politica de datos</p>
+            <p class="acerca-deFot">Acerca de</p>
+            <p class="condiciones-legales">Condiciones Legales</p>
+        </div>
+        <div class="group-7 justify-content-center mb-2">
+            <a href="https://twitter.com/elisj2002">
+                <img alt="" class="twitter"
+                    src="https://cdn.icon-icons.com/icons2/1/PNG/256/social_Twitter_38.png" /></a>
+            <a href="https://www.instagram.com/elisj2002/">
+                <img alt="" class="instagram"
+                    src="https://cdn.icon-icons.com/icons2/2428/PNG/512/instagram_black_logo_icon_147122.png" /></a>
+            <a href="https://www.linkedin.com/in/elisea-serrano-jim%C3%A9nez-2a9a07212/">
+                <img alt="" class="linkedin"
+                    src="https://cdn.icon-icons.com/icons2/693/PNG/512/LinkedIn_Rounded_Solid_icon-icons.com_61559.png" /></a>
+        </div>
     </div>
-    <div class="group-6 justify-content-center">
-      <p class="politica-de-datos">Politica de datos</p>
-      <p class="acerca-deFot">Acerca de</p>
-      <p class="condiciones-legales">Condiciones Legales</p>
-    </div>
-    <div class="group-7 justify-content-center">
-      <img alt="" class="twitter"
-        src="https://static.overlay-tech.com/assets/e7e6f611-bae3-47f6-a376-6c7d10315234.png" /><img alt=""
-        class="instagram" src="https://static.overlay-tech.com/assets/7e06e8b0-7c93-44d7-a7cd-0cd92498fb12.png" /><img
-        alt="" class="facebook" src="https://static.overlay-tech.com/assets/e498ff32-d882-4cfe-9459-d74d0f115e24.png" />
-    </div>
-  </div>
 </footer>
